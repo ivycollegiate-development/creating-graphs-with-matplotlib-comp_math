4907509928eba403cf5c434632c583f313043c98
PaulTJJ <pjones25_student@ivycollegiate.org>
2024-10-23 05:58:37 +0000

    installed jupyter, added madplotlib

--- PaulJJ_Sprint_Projectv2.3/PaulJJ_Sprint_Projectv2.3.py
+++ PaulJJ_Sprint_Projectv2.3/PaulJJ_Sprint_Projectv2.3.py
@@ -2,12 +2,13 @@ import sys
 from time import sleep
 from pylab import plot, show
 
-words = "This is just a test :P"
+words = "This is a test ;0;"
 for char in words:
     sleep(0.5)
-        sys.stdout.write(char)
+    sys.stdout.write(char)
 
 x_num = [1, 2, 3]
 y_num = [2, 4, 6]
 plot (x_num, y_num)
 show ()
+##git pull --rebase
