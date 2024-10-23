db6d01f53ed597a689c427565933a800cd3be835
PaulTJJ <pjones25_student@ivycollegiate.org>
2024-10-23 06:02:42 +0000

    Imported code block from docs

--- PaulJJ_Sprint_Projectv2.3/PaulJJ_Sprint_Projectv2.3.py
+++ PaulJJ_Sprint_Projectv2.3/PaulJJ_Sprint_Projectv2.3.py
@@ -12,3 +12,14 @@ y_num = [2, 4, 6]
 plot (x_num, y_num)
 show ()
 ##git pull --rebase
+
+import matplotlib.pyplot as plt
+nyc_temp = [53.9, 56.3, 56.4, 53.4, 54.5, 55.8, 56.8,
+55.0, 55.3, 54.0, 56.7, 56.4, 57.3]
+years = range(2000, 2013)
+plt.plot(years, nyc_temp, marker='o')
+plt.title("Average Annual Temperature in NYC (2000-2012)")
+plt.xlabel("Year")
+plt.ylabel("Temperature (°F)")
+plt.grid(True)
+plt.show()
